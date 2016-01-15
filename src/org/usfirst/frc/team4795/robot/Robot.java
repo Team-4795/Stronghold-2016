5ed296292dd8745cb76182b45e9a5acd49a7bf2b
Namr <amrmoussa100@gmail.com>
2016-01-15 16:28:41 -0500

    Drive Train Setup

--- src/org/usfirst/frc/team4795/robot/Robot.java
+++ src/org/usfirst/frc/team4795/robot/Robot.java
@@ -1,85 +1,76 @@
-
 package org.usfirst.frc.team4795.robot;
 
-import org.usfirst.frc.team4795.robot.commands.DriveForward;
+import org.usfirst.frc.team4795.robot.commands.TankDrive;
 import org.usfirst.frc.team4795.robot.subsystems.Drivetrain;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.tables.TableKeyNotDefinedException;
 
-
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
- */
 public class Robot extends IterativeRobot {
+	
+	private static final double PVAL = 0.0;
+	private static final double IVAL = 0.0;
+	private static final double DVAL = 0.0;
 
 	public static OI oi;
 	public static Drivetrain drivetrain;
-    Command autonomousCommand;
-    
-
-    /**
-     * This function is run when the robot is first started up and should be
-     * used for any initialization code.
-     */
-    public void robotInit() {
-    	drivetrain = new Drivetrain();
-		oi = new OI();
-        // instantiate the command used for the autonomous period
-    }
 	
+	@Override
+	public void robotInit() {
+		SmartDashboard.putNumber("P", PVAL);
+		SmartDashboard.putNumber("I", IVAL);
+		SmartDashboard.putNumber("D", DVAL);
+		
+		oi = new OI();
+		drivetrain = new Drivetrain();
+	}
+	
+	@Override
+	public void disabledInit() {}
+	
+	@Override
 	public void disabledPeriodic() {
 		Scheduler.getInstance().run();
 	}
-
-    public void autonomousInit() {
-        // schedule the autonomous command (example)
-        autonomousCommand = new DriveForward(3);
-    	autonomousCommand.start();
-    }
-
-    /**
-     * This function is called periodically during autonomous
-     */
-    public void autonomousPeriodic() {
-        Scheduler.getInstance().run();
-    }
-
-    public void teleopInit() {
-		// This makes sure that the autonomous stops running when
-        // teleop starts running. If you want the autonomous to 
-        // continue until interrupted by another command, remove
-        // this line or comment it out.
-        if (autonomousCommand != null) autonomousCommand.cancel();
-        drivetrain.startPercentMode();
-    }
-
-    /**
-     * This function is called when the disabled button is hit.
-     * You can use it to reset subsystems before shutting down.
-     */
-    public void disabledInit(){
-
-    }
-
-    /**
-     * This function is called periodically during operator control
-     */
-    public void teleopPeriodic() {
-        Scheduler.getInstance().run();
-        drivetrain.log();
-    }
-    
-    /**
-     * This function is called periodically during test mode
-     */
-    public void testPeriodic() {
-        LiveWindow.run();
-    }
+	
+	@Override
+	public void autonomousInit() {
+		/*
+		try {
+			double p = SmartDashboard.getNumber("P", 0.0);
+			double i = SmartDashboard.getNumber("I", 0.0);
+			double d = SmartDashboard.getNumber("D", 0.0);
+			Scheduler.getInstance().add(new PIDDrive(p, i, d));
+		} catch(TableKeyNotDefinedException | IllegalArgumentException ignored) {}
+		*/
+	}
+	
+	@Override
+	public void autonomousPeriodic() {
+		Scheduler.getInstance().run();
+	}
+	
+	@Override
+	public void teleopInit() {
+		Scheduler.getInstance().add(new TankDrive());
+	}
+	
+	@Override
+	public void teleopPeriodic() {
+		Scheduler.getInstance().run();
+	}
+	
+	@Override
+	public void testInit() {
+		
+	}
+	
+	@Override
+	public void testPeriodic() {
+		LiveWindow.run();
+	}
+	
 }
