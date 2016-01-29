104015f324f0aac92ad959c1a08ef1bf536aff86
Blackjak34 <castleberrysam@gmail.com>
2016-01-29 12:44:13 -0500

    Changed CAN IDs

--- src/org/usfirst/frc/team4795/robot/RobotMap.java
+++ src/org/usfirst/frc/team4795/robot/RobotMap.java
@@ -1,12 +1,12 @@
 package org.usfirst.frc.team4795.robot;
 	
 public enum RobotMap {
-	LEFT_MOTOR_1	(0),
-	LEFT_MOTOR_2	(1),
-	RIGHT_MOTOR_1	(2),
-	RIGHT_MOTOR_2	(3),
-	ARM_MOTOR 		(4),
-	INTAKE_MOTOR 	(5);
+	LEFT_MOTOR_1	(1),
+	LEFT_MOTOR_2	(2),
+	RIGHT_MOTOR_1	(3),
+	RIGHT_MOTOR_2	(4),
+	ARM_MOTOR 		(5),
+	INTAKE_MOTOR 	(6);
 	
 	public final int value;
 	
