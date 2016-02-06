6304e4227e0d34c4206a38d7f7ebc2296039edee
Blackjak34 <castleberrysam@gmail.com>
2016-02-05 21:41:16 -0500

    Added command for single joystick drive

--- src/org/usfirst/frc/team4795/robot/OI.java
+++ src/org/usfirst/frc/team4795/robot/OI.java
@@ -1,10 +1,19 @@
 package org.usfirst.frc.team4795.robot;
 
+import org.usfirst.frc.team4795.robot.commands.JoystickDrive;
+import org.usfirst.frc.team4795.robot.commands.TankDrive;
+
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 public class OI {
 
 	public final Joystick LEFTJOY = new Joystick(0);
 	public final Joystick RIGHTJOY = new Joystick(1);
+
+	public OI() {
+		new JoystickButton(RIGHTJOY, 3).whenPressed(new TankDrive());
+		new JoystickButton(LEFTJOY, 4).whenPressed(new JoystickDrive());
+	}
 
 }
